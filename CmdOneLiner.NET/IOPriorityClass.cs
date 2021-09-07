4122886c838d493c1fb53ddeaa5f62a38f99dc16
Adrian B <github@urriellu.net>
2021-09-07 18:27:56 +0200

    Adding IOPriorityClass.GetSimilarProcessPriority() and moving to System.IO namespace.

--- CmdOneLiner.NET/IOPriorityClass.cs
+++ CmdOneLiner.NET/IOPriorityClass.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
-namespace CmdOneLinerNET
+namespace System.IO
 {
     public enum IOPriorityClass
     {
@@ -13,5 +14,24 @@ namespace CmdOneLinerNET
         L04_Admin_RealTime_LowEffort,
         L05_Admin_RealTime_AverageEffort,
         L06_Admin_RealTime_ExtremelyHighEffort
+    }
+
+    public static class IOPriorityClassExtensions
+    {
+        public static ProcessPriorityClass GetSimilarProcessPriority(this IOPriorityClass ioPriorityClass)
+        {
+            switch (ioPriorityClass)
+            {
+                case IOPriorityClass.L00_Idle: return ProcessPriorityClass.Idle;
+                case IOPriorityClass.L01_LowEffort: return ProcessPriorityClass.BelowNormal;
+                case IOPriorityClass.L02_NormalEffort: return ProcessPriorityClass.Normal;
+                case IOPriorityClass.L03_HighEffort: return ProcessPriorityClass.AboveNormal;
+                case IOPriorityClass.L04_Admin_RealTime_LowEffort: return ProcessPriorityClass.High;
+                case IOPriorityClass.L05_Admin_RealTime_AverageEffort:
+                case IOPriorityClass.L06_Admin_RealTime_ExtremelyHighEffort:
+                    return ProcessPriorityClass.RealTime;
+                default: throw new NotImplementedException(ioPriorityClass.ToString());
+            }
+        }
     }
 }
