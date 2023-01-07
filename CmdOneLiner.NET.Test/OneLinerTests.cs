766d29988627a6a2a8b776bbc6057dea2a04af66
Adrian B <github@urriellu.net>
2023-01-07 21:57:18 +0100

    New stress test + bug fixes

--- CmdOneLiner.NET.Test/OneLinerTests.cs
+++ CmdOneLiner.NET.Test/OneLinerTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace CmdOneLinerNET.Test
@@ -7,23 +9,34 @@ namespace CmdOneLinerNET.Test
     [TestClass]
     public class OneLinerTests
     {
+        private readonly string nameSleeperBinary = System.IO.File.Exists("Sleeper.exe") ? "Sleeper.exe" : "Sleeper";
+
         [TestMethod, Timeout(10 * 1000)]
-        public void T01_BasicTest()
+        public void T01_BasicTest(bool multithreaded = false)
         {
-            (int ExitCode, bool Success, string StdOut, string StdErr, long? MaxRamUsedBytes, TimeSpan? UserProcessorTime, TimeSpan? TotalProcessorTime) = CmdOneLiner.Run($"Sleeper.exe 1");
+            (int ExitCode, bool Success, string StdOut, string StdErr, long? MaxRamUsedBytes, TimeSpan? UserProcessorTime, TimeSpan? TotalProcessorTime) = CmdOneLiner.Run($"{nameSleeperBinary} 1");
             Assert.AreEqual(ExitCode, 0);
             Assert.AreEqual(Success, true);
-            Assert.IsTrue(MaxRamUsedBytes > 0 && MaxRamUsedBytes < 50 * 1000 * 1000);
-            Assert.IsTrue(UserProcessorTime > TimeSpan.Zero);
+            if (multithreaded)
+            {
+                Assert.IsTrue(MaxRamUsedBytes >= 0 && MaxRamUsedBytes < 50 * 1000 * 1000);
+                Assert.IsTrue(UserProcessorTime >= TimeSpan.Zero);
+                Assert.IsTrue(TotalProcessorTime >= TimeSpan.Zero);
+            }
+            else
+            {
+                Assert.IsTrue(MaxRamUsedBytes > 0 && MaxRamUsedBytes < 50 * 1000 * 1000);
+                Assert.IsTrue(UserProcessorTime > TimeSpan.Zero);
+                Assert.IsTrue(TotalProcessorTime > TimeSpan.Zero);
+            }
             Assert.IsTrue(UserProcessorTime < TimeSpan.FromSeconds(5));
-            Assert.IsTrue(TotalProcessorTime > TimeSpan.Zero);
             Assert.IsTrue(TotalProcessorTime < TimeSpan.FromSeconds(5));
         }
 
         [TestMethod, Timeout(10 * 1000)]
         public void T02_TimeoutTest()
         {
-            (int ExitCode, bool Success, string StdOut, string StdErr, long? MaxRamUsedBytes, TimeSpan? UserProcessorTime, TimeSpan? TotalProcessorTime) = CmdOneLiner.Run($"Sleeper.exe 10", timeout: TimeSpan.FromSeconds(1));
+            (int ExitCode, bool Success, string StdOut, string StdErr, long? MaxRamUsedBytes, TimeSpan? UserProcessorTime, TimeSpan? TotalProcessorTime) = CmdOneLiner.Run($"{nameSleeperBinary} 10", timeout: TimeSpan.FromSeconds(1));
             Assert.AreNotEqual(ExitCode, 0);
             Assert.AreEqual(Success, false);
             Assert.IsTrue(UserProcessorTime < TimeSpan.FromSeconds(5));
@@ -31,23 +44,45 @@ namespace CmdOneLinerNET.Test
         }
 
         [TestMethod, Timeout(10 * 1000)]
-        public void T03_KillProcessTest()
+        public void T03_KillProcessTest(bool multithreaded = false)
         {
             System.Threading.CancellationTokenSource cancelSrc = new System.Threading.CancellationTokenSource();
             System.Threading.CancellationToken token = cancelSrc.Token;
 
             // Create a new thread/task that will cancel the command after 3 seconds
+            Debug.WriteLine($"Starting at {DateTime.Now}");
             Task.Delay(TimeSpan.FromSeconds(3)).ContinueWith(t =>
             {
+                Debug.WriteLine($"Canceling at {DateTime.Now}");
                 cancelSrc.Cancel();
             });
 
-            (int ExitCode, bool Success, string StdOut, string StdErr, long? MaxRamUsedBytes, TimeSpan? UserProcessorTime, TimeSpan? TotalProcessorTime) = CmdOneLiner.Run($"Sleeper.exe 60", canceltoken: token);
+            Stopwatch execTime = Stopwatch.StartNew();
+            (int ExitCode, bool Success, string StdOut, string StdErr, long? MaxRamUsedBytes, TimeSpan? UserProcessorTime, TimeSpan? TotalProcessorTime) = CmdOneLiner.Run($"{nameSleeperBinary} 60", canceltoken: token);
+            execTime.Stop();
             Assert.AreNotEqual(ExitCode, 0);
             Assert.AreEqual(Success, false);
-            Assert.IsTrue(UserProcessorTime > TimeSpan.FromSeconds(1));
+            if (multithreaded) Assert.IsTrue(UserProcessorTime >= TimeSpan.FromSeconds(0));
+            else Assert.IsTrue(UserProcessorTime > TimeSpan.FromSeconds(0));
             Assert.IsTrue(UserProcessorTime < TimeSpan.FromSeconds(10));
+            Assert.IsTrue(execTime.Elapsed >= TimeSpan.FromSeconds(3));
+            Assert.IsTrue(execTime.Elapsed < TimeSpan.FromSeconds(multithreaded ? 120 : 10));
             Assert.IsTrue(StdErr.Contains("killed"));
+        }
+
+        [TestMethod, Timeout(5 * 60 * 1000)]
+        public void T04_StressTest()
+        {
+            const int amountThreadsPerTest = 5;
+            TimeSpan runFor = TimeSpan.FromSeconds(30);
+            Stopwatch runningFor = Stopwatch.StartNew();
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < amountThreadsPerTest; i++) {
+                tasks.Add(Task.Factory.StartNew(() => { while(runningFor.Elapsed < runFor) T01_BasicTest(multithreaded: true); }));
+                tasks.Add(Task.Factory.StartNew(() => { while(runningFor.Elapsed < runFor) T02_TimeoutTest(); }));
+                tasks.Add(Task.Factory.StartNew(() => { while(runningFor.Elapsed < runFor) T03_KillProcessTest(multithreaded: true); }));
+            }
+            Task.WaitAll(tasks.ToArray());
         }
     }
 }
